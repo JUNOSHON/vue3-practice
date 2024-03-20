a082cb33cbeeecc2873f827b80a5f24fdb3cb7dd
junoshon <sanoldfox@naver.com>
2024-03-20 20:16:58 +0900

    반응형 언래핑

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="log">{{ msg }}</button>
+    <h3>{{capitalize('hello')}}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { capitalize } from 'vue';
+
+const msg = '안녕!';
+
+function log() {
+  console.log(msg);
+}
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CounterPage from '../pages/CounterPage.vue';
-import LoginPage from '../pages/LoginPage.vue';
+import IndexPage from '../pages/IndexPage.vue';
 
 const routes = [
   {
     path: '/',
-    redirect: '/login',
-  },
-  {
-    path: '/login',
-    component: LoginPage,
-  },
-  {
-    path: '/counter',
-    component: CounterPage,
+    component: IndexPage,
   },
 ];
 
